409f15cf439faa2ac869ab2ef32dcec192e87cf7
Đăng Nghẽo <140532323+nyclone569@users.noreply.github.com>
2025-02-24 00:53:19 +0700

    config enable CORS

--- server/index.js
+++ server/index.js
@@ -1,26 +1,20 @@
 import express from 'express'
 import path from 'path'
-import cors from 'cors'
 
 const app = express()
 
-const corsOptions = {
-    origin: '*', 
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type',
-};
-
-app.use(cors(corsOptions));
-
 app.use(express.static(path.resolve('../dist')));
+
+app.all('*', (req, res, next) => {
+    res.sendFile(path.resolve('../dist/index.html'))
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+})
 
 app.get('/elearning/v4/courses', (req, res) => {
     res.json({ message: 'This is the API response.' });
 });
-
-app.all('*', (req, res) => {
-    res.sendFile(path.resolve('../dist/index.html'))
-})
 
 app.listen(3000, () => {
     console.log('Server Nodejs listen on port 3000')
