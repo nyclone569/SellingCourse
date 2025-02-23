01bcb4fdec4f2d726a3de6236b967ce434d47269
Đăng Nghẽo <140532323+nyclone569@users.noreply.github.com>
2025-02-24 01:22:38 +0700

    testing on update vercel.json

--- server/index.js
+++ server/index.js
@@ -1,22 +1,10 @@
 import express from 'express'
 import path from 'path'
-import cors from 'cors'
 
 const app = express()
 
-const corsOptions = {
-    origin: '*', 
-    credentials:true,      
-    optionSuccessStatus:200,
-};
+app.use(express.static('../dist'));
 
-app.use(cors(corsOptions));
-
-app.use(express.static(path.resolve('../dist')));
-
-// app.get('/elearning/v4/courses', (req, res) => {
-//     res.json({ message: 'This is the API response.' });
-// });
 
 app.all('*', (req, res) => {
     res.sendFile(path.resolve('../dist/index.html'))
