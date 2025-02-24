ea6f4a7c2d0f5ba08218ad8f2e2428590b031bf9
Đăng Nghẽo <140532323+nyclone569@users.noreply.github.com>
2025-02-24 18:04:12 +0700

    testing on netlify functions

---
+++ netlify/functions/apiProxy.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+
+exports.handler = async (event, context) => {
+  try {
+    const token = getToken()
+    const response = await axios({
+      method: 'POST', 
+      url: 'https://course.spacedev.vn/users/endpoint', 
+      data: JSON.parse(event.body),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token.accessToken}`,
+      },
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
