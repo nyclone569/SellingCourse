c0b841b516a4522225ece20e6b0a9c0b1673ec78
nyclone569 <nyclone569@gmail.com>
2025-08-22 14:15:31 +0700

    Use Netlify functions to fix cors error

--- netlify/functions/apiProxy.js
+++ netlify/functions/apiProxy.js
@@ -1,16 +1,43 @@
-import axios from "axios"
+const axios = require('axios');
 
 exports.handler = async (event, context) => {
+  // Handle preflight requests
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      },
+      body: '',
+    };
+  }
+
   try {
+    // Extract the target URL path and parameters
+    const path = event.path.replace('/.netlify/functions/apiProxy', '');
+    const targetUrl = `https://course.spacedev.vn${path}`;
+    
+    // Prepare headers (remove Netlify-specific headers)
+    const headers = {};
+    if (event.headers) {
+      // Forward important headers
+      if (event.headers['content-type']) {
+        headers['Content-Type'] = event.headers['content-type'];
+      }
+      if (event.headers['authorization']) {
+        headers['Authorization'] = event.headers['authorization'];
+      }
+    }
+
     // Forward the request to the external API
     const response = await axios({
       method: event.httpMethod,
-      url: 'https://course.spacedev.vn/authentication/v2/login',
+      url: targetUrl,
       data: event.body ? JSON.parse(event.body) : undefined,
-      headers: {
-        'Content-Type': 'application/json',
-        ...event.headers, // Forward headers from the client
-      },
+      headers: headers,
+      params: event.queryStringParameters,
     });
 
     // Return the response from the external API
@@ -24,6 +51,8 @@ exports.handler = async (event, context) => {
       body: JSON.stringify(response.data),
     };
   } catch (error) {
+    console.error('API Proxy Error:', error);
+    
     return {
       statusCode: error.response?.status || 500,
       headers: {
